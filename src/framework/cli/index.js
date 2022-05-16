fe5b3fe13b77291c0011c95221d8ce6ee09d1b70
Chayanan Tantasirin <chayanan.t@i3gateway.com>
2022-05-17 01:07:53 +0700

    Add cli for read and instruction file for init rover position and direction

---
+++ src/framework/cli/index.js
@@ -0,0 +1,43 @@
+import * as readline from 'readline'
+import fs from 'fs'
+import path from 'path';
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+export default (dependencies) => {
+    const {
+        roverController
+    } = dependencies
+
+    if (!roverController) {
+        throw new Error('roverController should be in depencies')
+    }
+    const execute = () => {
+        return new Promise((resolve) => {
+            rl.question('Fill in your file name: ', async (fileName) => {
+                const filePath = path.resolve('storage', fileName)
+                try {
+                    if (fs.existsSync(filePath)) {
+                        const fileData = fs.readFileSync(filePath, 'utf8')
+                        const lines = fileData.split('\n')
+                        const actionHistories = roverController.initRoverByInstruction(dependencies).execute(lines)
+                        for (const actionHistory of actionHistories) {
+                            console.log(actionHistory.action,
+                                '  |  ',
+                                `${actionHistory.roverData.direction}:${actionHistory.roverData.xPos},${actionHistory.roverData.yPos}`)
+                        }
+                    } else {
+                        console.log("I can't found file name or directory please try again.")
+                        await execute()
+                    }
+                } catch (err) {
+                    console.error(err.validationErrors.msg)
+                    await execute()
+                }
+
+                resolve()
+            });
+        })
+    }
+
+    return { execute }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import dependencies from "./config/dependency.js"
+import cli from "./framework/cli/index.js"
+
 export const main = {
     start: async () => {
-        console.log("Hello world!")
+        console.log('You have a mission to discover mars with rover! 🚀 🚀 🚀')
+        console.log('Please put your move instruction in /storage path.')
+        await cli(dependencies).execute()
     }
 }
